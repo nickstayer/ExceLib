fee063f5c8f9bbd2da2ac0617b67768c9934bca6
nickstayer <dubovskov@yandex.ru>
2024-08-23 12:08:47 +0500

    downgrade until 4.8 .dot net framework

---
+++ ExceLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExceLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExceLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("827a0ceb-8bf1-4afd-b009-aa6cb5fee9ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- ExceLib.Tests/Tests.cs
+++ ExceLib.Tests/Tests.cs
@@ -1,71 +1,75 @@
 using ExceLib;
+using NUnit.Framework;
+using System.IO;
 
-namespace Tests;
-
-public class Tests
+namespace ExceLib.Tests
 {
-    ExcelApp? _excel;
-    string? _dataDir;
-
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-        _excel = new ExcelApp(false);
-        _dataDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\data\"));
-    }
+        ExcelApp _excel;
+        string _dataDir;
 
-    [Test]
-    public void TestOpenFile()
-    {
-        var testFile = _dataDir + "test.xlsx";
-        if (!File.Exists(testFile)) 
+        [SetUp]
+        public void Setup()
         {
-            throw new FileNotFoundException(testFile);
+            _excel = new ExcelApp(false);
+            _dataDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\data\"));
         }
-        _excel?.OpenDoc(testFile);
-        Assert.DoesNotThrow(() => _excel?.OpenDoc(testFile));
-    }
 
-    [Test]
-    [TestCase("B2", "Просто текст")]
-    [TestCase("C2", "5")]
-    [TestCase("D2", " ")]
-    [TestCase("E2", null)]
-    [TestCase("F2", "01.01.2001 0:00:00")]
-    public void TestGetValue(string cell, string? expected)
-    {
-        var testFile = _dataDir + "test.xlsx";
-        if (!File.Exists(testFile))
+        [Test]
+        public void TestOpenFile()
         {
-            throw new FileNotFoundException(testFile);
+            var testFile = _dataDir + "test.xlsx";
+            if (!File.Exists(testFile))
+            {
+                throw new FileNotFoundException(testFile);
+            }
+            _excel?.OpenDoc(testFile);
+            Assert.DoesNotThrow(() => _excel?.OpenDoc(testFile));
         }
-        _excel?.OpenDoc(testFile);
-        var actual = _excel?.GetValue(cell)?.ToString();
-        Assert.That(actual, Is.EqualTo(expected));
-    }
 
-    [Test]
-    [TestCase("B2", "Просто текст")]
-    [TestCase("C2", "5")]
-    [TestCase("D2", " ")]
-    [TestCase("E2", null)]
-    [TestCase("F2", "36892")]
-    public void TestGetValue2(string cell, string? expected)
-    {
-        var testFile = _dataDir + "test.xlsx";
-        if (!File.Exists(testFile))
+        [Test]
+        [TestCase("B2", "Просто текст")]
+        [TestCase("C2", "5")]
+        [TestCase("D2", " ")]
+        [TestCase("E2", null)]
+        [TestCase("F2", "01.01.2001 0:00:00")]
+        public void TestGetValue(string cell, string expected)
         {
-            throw new FileNotFoundException(testFile);
+            var testFile = _dataDir + "test.xlsx";
+            if (!File.Exists(testFile))
+            {
+                throw new FileNotFoundException(testFile);
+            }
+            _excel?.OpenDoc(testFile);
+            var actual = _excel?.GetValue(cell)?.ToString();
+            Assert.That(actual, Is.EqualTo(expected));
         }
-        _excel?.OpenDoc(testFile);
-        var actual = _excel?.GetValue2(cell)?.ToString();
-        Assert.That(actual, Is.EqualTo(expected));
-    }
 
-    [TearDown]
-    public void TearDown()
-    {
-        _excel?.Quit();
-    }
+        [Test]
+        [TestCase("B2", "Просто текст")]
+        [TestCase("C2", "5")]
+        [TestCase("D2", " ")]
+        [TestCase("E2", null)]
+        [TestCase("F2", "36892")]
+        public void TestGetValue2(string cell, string expected)
+        {
+            var testFile = _dataDir + "test.xlsx";
+            if (!File.Exists(testFile))
+            {
+                throw new FileNotFoundException(testFile);
+            }
+            _excel?.OpenDoc(testFile);
+            var actual = _excel?.GetValue2(cell)?.ToString();
+            Assert.That(actual, Is.EqualTo(expected));
+        }
 
+        [TearDown]
+        public void TearDown()
+        {
+            _excel?.Quit();
+        }
+
+    }
 }
+
